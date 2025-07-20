946eead72dd91c75ce79c752b6fed4c21f692ecd
zGeanX <geanlucadias12@gmail.com>
2025-07-20 18:20:08 -0300

    fix permissao do CORS para acessar o Vercel

--- config/settings.py
+++ config/settings.py
@@ -121,4 +121,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = [
+    "https://sistema-de-folgas.vercel.app",
+    "https://localgost:3000",
+]
