2d149069a3233ad3b107309b144403cf2e8994c0
zGeanX <geanlucadias12@gmail.com>
2025-07-21 11:16:20 -0300

    fix exibi;cao do admin

--- escala/admin.py
+++ escala/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import SolicitacaoFolga
 
-admin.site.register(SolicitacaoFolga)
+@admin.register(SolicitacaoFolga)
+class SolicitacaoFolgaAdmin(admin.ModelAdmin):
+    list_display = ('cartomante_nome', 'dia_semana', 'turno', 'status', 'data_solicitacao')
+
+    list_filter = ('status', 'dia_semana', 'turno')
+
+    search_fields = ('cartomante_nome',)
+
+    list_editable = ('status',)
