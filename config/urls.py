53b7446e60639efc1d27ff7744e9ea69b0517968
zGeanX <geanlucadias12@gmail.com>
2026-02-03 22:33:57 -0300

    update: manage.py and config urls

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('escala.urls')),
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/', include('apps.users.api.urls')),
+    path('api/', include('apps.escala.api.urls')),
 ]
