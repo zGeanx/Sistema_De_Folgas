c91f0eec3fe97609b0aa0d81d675d971c3b65bfc
zGeanX <geanlucadias12@gmail.com>
2026-08-16 13:06:14 -0300

    chore(backend): remove legacy root escala app directory

--- escala/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import SolicitacaoFolga
-
-class SolicitacaoFolgaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SolicitacaoFolga
-        fields = ['id', 'cartomante_nome', 'dia_semana', 'turno', 'status', 'data_solicitacao']
-        read_only_fields = ['status'] # Apenas o status é somente leitura na criação
--- escala/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets, permissions
-from .models import SolicitacaoFolga
-from .serializers import SolicitacaoFolgaSerializer
-
-class SolicitacaoFolgaViewSet(viewsets.ModelViewSet):
-    queryset = SolicitacaoFolga.objects.all().order_by('-data_solicitacao')
-    serializer_class = SolicitacaoFolgaSerializer
-    
-    permission_classes = [permissions.AllowAny]
