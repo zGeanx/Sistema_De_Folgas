5b5f31478c4d91ce3e745c1a540915d51556b973
zGeanX <geanlucadias12@gmail.com>
2026-08-16 13:05:56 -0300

    refactor(settings): align INSTALLED_APPS with apps structure

--- config/settings.py
+++ config/settings.py
@@ -26,10 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'corsheaders',
-    'escala',
-    
-    ]
+    'apps.users',
+    'apps.escala',
+]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
