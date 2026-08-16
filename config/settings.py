d5ab166bc25f1b1b494d0299e58a9bcd193000d9
zGeanX <geanlucadias12@gmail.com>
2026-08-16 14:26:16 -0300

    chore(deploy): add cloudflare redirects, procfile and cors setup

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-um-valor-segu
 
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -111,8 +111,12 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.pages\.dev$",
+]
 CORS_ALLOWED_ORIGINS = [
-     "https://sistema-de-folgas.vercel.app",
+    "https://sistema-de-folgas.vercel.app",
     "http://localhost:3000",
 ]
 
