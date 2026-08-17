d09034e6e9cacbe10ed5987b9061932e7fdbef3d
zGeanX <geanlucadias12@gmail.com>
2026-08-16 22:38:58 -0300

    fix(config): require explicit production hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [host for host in os.environ.get('ALLOWED_HOSTS', '').split(',') if host]
 
 database_url = os.environ.get('DATABASE_URL')
 use_sqlite = os.environ.get('USE_SQLITE', 'false').lower() == 'true'
@@ -25,10 +25,8 @@ else:
         }
     }
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://.*\.pages\.dev$",
+CORS_ALLOWED_ORIGINS = [
+    origin for origin in os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',') if origin
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
